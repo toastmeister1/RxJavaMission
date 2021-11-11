d8644b9f54241612c65f2587e988c973ce6834b8
Joseph <wjsdudtkd95@gmail.com>
2021-11-11 15:08:13 +0900

    feat(base): initial commit

---
+++ app/src/main/java/knowre/android/rxjavamission/MainActivity.kt
@@ -0,0 +1,21 @@
+package knowre.android.rxjavamission
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import dagger.hilt.android.AndroidEntryPoint
+import knowre.android.rxjavamission.databinding.ActivityMainBinding
+
+@AndroidEntryPoint
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+}
---
+++ app/src/main/java/knowre/android/rxjavamission/MainViewModel.kt
@@ -0,0 +1,9 @@
+package knowre.android.rxjavamission
+
+import androidx.lifecycle.ViewModel
+import rx.subjects.BehaviorSubject
+import java.util.concurrent.TimeUnit
+
+
+internal class MainViewModel : ViewModel() {
+}
