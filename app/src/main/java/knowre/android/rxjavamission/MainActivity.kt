84abfba8e48b2ed2ddcb7fe39112d3284f1aabf1
Joseph <wjsdudtkd95@gmail.com>
2021-11-11 16:36:22 +0900

    feat(base): textWatcher + RxJava 구현

--- app/src/main/java/knowre/android/rxjavamission/MainActivity.kt
+++ app/src/main/java/knowre/android/rxjavamission/MainActivity.kt
@@ -4,19 +4,61 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import androidx.activity.viewModels
 import dagger.hilt.android.AndroidEntryPoint
 import knowre.android.rxjavamission.databinding.ActivityMainBinding
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.decodeFromJsonElement
+import kotlinx.serialization.json.encodeToJsonElement
 import rx.Observable
+import timber.log.Timber
+import java.util.concurrent.TimeUnit
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    private val viewModel by viewModels<MainViewModel>()
+
+    companion object {
+        const val INPUT_DELAY = 300L
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        initializeQueryEditText()
+        initializeSubscribe()
+    }
+
+    private fun initializeSubscribe() = with(viewModel) {
+        query.subscribe {
+            Timber.d("[Query] : $it")
+        }
+    }
+
+    private fun initializeQueryEditText() = with(binding) {
+        val queryObservable = Observable.unsafeCreate<String> { emitter ->
+            etQuery.addTextChangedListener(object : TextWatcher {
+                override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) = Unit
+                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                    emitter.onNext(s.toString())
+                }
+
+                override fun afterTextChanged(s: Editable?) = Unit
+            })
+        }
+
+        queryObservable.debounce(INPUT_DELAY, TimeUnit.MILLISECONDS)
+            .filter { query -> query.isNotEmpty() }
+            .subscribe { query ->
+                viewModel.handleEvent(MainViewModel.Event.OnQueryTextChanged(query))
+            }
     }
 
 }
--- app/src/main/java/knowre/android/rxjavamission/MainViewModel.kt
+++ app/src/main/java/knowre/android/rxjavamission/MainViewModel.kt
@@ -6,4 +6,17 @@ import java.util.concurrent.TimeUnit
 
 
 internal class MainViewModel : ViewModel() {
+
+    private val _query = BehaviorSubject.create<String>()
+    val query get() = _query.asObservable()
+
+    fun handleEvent(event: Event) = when (event) {
+        is Event.OnQueryTextChanged -> {
+            _query.onNext(event.value)
+        }
+    }
+
+    sealed class Event {
+        class OnQueryTextChanged(val value: String) : Event()
+    }
 }
